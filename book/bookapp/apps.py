41d30229742ffde6d3960785343765c69f58ddd6
yokesh27-06 <appuyokesh93@gmail.com>
2025-11-10 02:47:15 +0530

    sucess

--- book/bookapp/apps.py
+++ book/bookapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BcappConfig(AppConfig):
+class BookappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bcapp'
+    name = 'bookapp'
